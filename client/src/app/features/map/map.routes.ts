1a2906df2bd7bb6b396fed2cb8e7c5b92831e005
ame7cd <meinkenadrian@gmail.com>
2024-05-27 21:40:24 +0200

    add leaflet map

---
+++ client/src/app/features/map/map.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { MapViewComponent } from './components/map-view/map-view.component';
+
+export const mapRoutes: Routes = [
+  {
+    path: '',
+    component: MapViewComponent,
+  },
+];
