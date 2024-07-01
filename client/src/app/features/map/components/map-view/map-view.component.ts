50a203ee2a40f24dbfa12f3e3b933696daff8b14
ame7cd <meinkenadrian@gmail.com>
2024-07-01 22:48:15 +0200

    install tailwind, daisy, flowbite

--- client/src/app/features/map/components/map-view/map-view.component.ts
+++ client/src/app/features/map/components/map-view/map-view.component.ts
@@ -1,32 +1,39 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { StyleClassModule } from 'primeng/styleclass';
 import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map-view',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, StyleClassModule],
   templateUrl: './map-view.component.html',
-  styleUrl: './map-view.component.scss'
+  styleUrl: './map-view.component.scss',
 })
 export class MapViewComponent implements AfterViewInit {
-  private map: any;
+  @ViewChild('map') map: any;
+  activeTab: number = 0;
 
   private initMap(): void {
     this.map = L.map('map', {
-      center: [ 39.8282, -98.5795 ],
-      zoom: 3
+      center: [39.8282, -98.5795],
+      zoom: 3,
     });
 
-    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 18,
-      minZoom: 3,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    });
+    const tiles = L.tileLayer(
+      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      {
+        maxZoom: 18,
+        minZoom: 3,
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      }
+    );
 
     tiles.addTo(this.map);
   }
 
-  constructor() { }
+  constructor() {}
 
   ngAfterViewInit(): void {
     this.initMap();
